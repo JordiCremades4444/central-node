ed4d01c097442c7345c2f1777e225f5d67d01cec
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-09-09 15:29:40 +0200

    events tracker. outputs in csv. full mode ddps tracker

---
+++ notebooks/2024_9_ddp_tracker/queries/pna_bot_gmv_info.sql
@@ -0,0 +1,3 @@
+select 
+    count(*) as count_pna_bot_gmv_info
+from delta.central__pna_bot__odp.pna_bot_gmv_info
---
+++ notebooks/2024_9_ddp_tracker/queries/replacement_engine_info.sql
@@ -0,0 +1,3 @@
+select 
+    count(*) as count_replacement_engine_info
+from delta.central__replacement_engine_dp__odp.replacement_engine_info
