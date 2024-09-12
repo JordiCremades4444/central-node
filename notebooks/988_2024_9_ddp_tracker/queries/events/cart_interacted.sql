3072773a20defb5baca9966c1c6140a22ae7757c
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-09-12 18:50:21 +0200

    added tracking for cart event

---
+++ notebooks/988_2024_9_ddp_tracker/queries/events/cart_interacted.sql
@@ -0,0 +1,22 @@
+with calendar_dates as (select
+        calendar_date
+    from unnest(sequence(
+        date_add('day',-{days_in_advance},current_date),-- initial date
+        date_add('day',-1,current_date),-- final date
+        interval '1' day-- step
+    )) as dates (calendar_date)
+    where 1=1
+)
+
+select 
+    'cart' as project_name
+    ,date(creation_date) as p_creation_date
+    ,event_name
+    ,custom_attributes__cart_interaction_type as cart_interaction_type
+    ,count(distinct event_id) as count_event
+from sensitive_delta.customer_mpcustomer_odp.custom_event 
+where true
+    and creation_date in (select calendar_date from calendar_dates)
+    and (event_name = 'Cart Interacted')
+group by 1,2,3,4
+order by 1 asc
---
+++ notebooks/988_2024_9_ddp_tracker/queries/events/product_removed.sql
@@ -0,0 +1,23 @@
+with calendar_dates as (select
+        calendar_date
+    from unnest(sequence(
+        date_add('day',-{days_in_advance},current_date),-- initial date
+        date_add('day',-1,current_date),-- final date
+        interval '1' day-- step
+    )) as dates (calendar_date)
+    where 1=1
+)
+
+select 
+    'cart' as project_name
+    ,date(creation_date) as p_creation_date
+    ,event_name
+    ,custom_attributes__product_removed_origin as product_removed_origin
+    ,count(distinct event_id) as count_event
+from sensitive_delta.customer_mpcustomer_odp.commerce_event 
+where true
+    and creation_date in (select calendar_date from calendar_dates)
+    and (event_name = 'eCommerce - RemoveFromCart')
+    and custom_attributes__product_removed_origin = 'Cart Minus Button'
+group by 1,2,3,4
+order by 1 asc
