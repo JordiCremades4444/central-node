baf71a4d5ab67c7077b279fc57bbfabf595e45ef
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-09-16 21:03:08 +0200

    minimalism query engines

--- src/query_engines.py
+++ src/query_engines.py
@@ -1,292 +1,182 @@
 import json
-import matplotlib.pyplot as plt
-import mysql.connector
-import numpy as np
 import os
 import pandas as pd
+import mysql.connector
 import trino
-
 from datetime import datetime
-from sqlalchemy import create_engine
 
 
 class QueryEngines:
+    """
+    A class to manage SQL queries and interactions with databases.
+
+    Attributes:
+        shared_input (str): Path to the credentials JSON file.
+        to_load_path (str): Directory path for loading data.
+        query_logs_path (str): Directory path for query logs.
+        credentials (dict): Database credentials loaded from JSON.
+        query_file (str): SQL query file name.
+        params (list of dict): Query parameters to replace.
+        to_load_file (str): CSV file name to save query results.
+        load_from_to_load_file (str): CSV file name to load data from.
+        read_query (str): The raw SQL query read from file.
+        tp__read_query (str): The SQL query with parameters replaced.
+    """
+
     def __init__(self):
         """
-        Initializes the QueryEngines object, setting up credentials and connections.
+        Initializes the QueryEngines object by loading credentials and setting paths.
         """
-        try:
-            self.shared_input = (
-                "C:/Users/Jordi Cremades/Documents/Repos/central-node/credentials.json"
-            )
+        self.shared_input = (
+            "C:/Users/Jordi Cremades/Documents/Repos/central-node/credentials.json"
+        )
+        self.to_load_path = os.path.join(os.getcwd(), "to_load")
+        self.query_logs_path = os.path.join(os.getcwd(), "query_logs")
+        self._load_credentials()
 
-            # Load credentials
-            with open(self.shared_input, "r") as f:
-                self.credentials = json.load(f)
-
-            # Set default paths
-            self.to_load_path = os.path.join(os.getcwd(), "to_load")
-            self.query_logs_path = os.path.join(os.getcwd(), "query_logs")
-
-        except Exception as e:
-            print(f"An error occurred during initialization: {e}")
-
-    ####################################
-    # -- Ensure directory is prepared --#
-    ####################################
+    def _load_credentials(self):
+        """
+        Loads database credentials from the JSON file.
+        """
+        with open(self.shared_input, "r") as f:
+            self.credentials = json.load(f)
 
     def _ensure_directory(self, path):
         """
-        Ensures the directory exists.
+        Ensures that the specified directory exists.
 
         Parameters:
-        path (str): Path to the directory.
+            path (str): Directory path to check or create.
         """
         if not os.path.exists(path):
-            os.mkdir(path)
-
-    ##################################
-    # -- Query Preparation Section --#
-    ##################################
+            os.makedirs(path)
 
     def prepare_query(
-        self,
-        query_file,
-        params=None,
-        to_load_file=None,
-        load_from_to_load_file=None,
+        self, query_file, params=None, to_load_file=None, load_from_to_load_file=None
     ):
         """
-        Prepares the query by replacing parameters if needed and handles directory setup.
+        Prepares the SQL query by reading from a file and replacing parameters.
 
         Parameters:
-        ------------
-        query_file: String
-            Name of the SQL query file.
-        params: Dictionary
-            Dictionary with parameters to be replaced in the query.
-        to_load_file: String
-            If not null, the name of the CSV to save the query results.
-        load_from_output_file: String
-            If not null, the name of the CSV file to load data from instead of executing the query.
+            query_file (str): Name of the SQL query file.
+            params (list of dict, optional): Parameters to replace in the query.
+            to_load_file (str, optional): CSV file name to save query results.
+            load_from_to_load_file (str, optional): CSV file name to load data from.
         """
-        try:
-            self.query_file = query_file
-            self.params = params
-            self.to_load_file = to_load_file
-            self.load_from_to_load_file = load_from_to_load_file
-
-            # Ensure directories exist
-            self._ensure_directory(self.to_load_path)
-            self._ensure_directory(self.query_logs_path)
-
-            # Read and prepare query
-            self.qpath = os.path.join(os.getcwd(), "queries", self.query_file)
-            with open(self.qpath, "r") as f:
-                self.read_query = f.read()
-
-            self.tp__read_query = self.replace_params()  # read query and replace params
-
-        except Exception as e:
-            print(f"An error occurred while preparing the query: {e}")
+        self.query_file = query_file
+        self.params = params
+        self.to_load_file = to_load_file
+        self.load_from_to_load_file = load_from_to_load_file
+        self._ensure_directory(self.to_load_path)
+        self._ensure_directory(self.query_logs_path)
+        with open(os.path.join(os.getcwd(), "queries", self.query_file), "r") as f:
+            self.read_query = f.read()
+        self.tp__read_query = self.replace_params()
 
     def replace_params(self):
         """
-        Replaces parameters in the query with provided values.
+        Replaces parameters in the SQL query with provided values.
 
         Returns:
-        ------------
-        String
-            Returns the query with parameters replaced.
+            str: The SQL query with parameters replaced.
         """
-        try:
-            tp__read_query = self.read_query
-            # Replace parameters
-            if self.params:
-                for param in self.params:
-                    param_in_sql = "{" + str(param["name"]) + "}"
-                    tp__read_query = tp__read_query.replace(
-                        param_in_sql, param["value"]
-                    )
-            return tp__read_query
-        except Exception as e:
-            print(f"An error occurred while replacing parameters: {e}")
-
-    ##################################
-    # -- CSV Handling Section --#
-    ##################################
+        if self.params:
+            for param in self.params:
+                self.read_query = self.read_query.replace(
+                    f"{{{param['name']}}}", param["value"]
+                )
+        return self.read_query
 
     def load_from_csv(self, file_name):
         """
         Loads data from a CSV file into a DataFrame.
 
         Parameters:
-        ------------
-        file_name: String
-            The name of the CSV file to load data from.
+            file_name (str): Name of the CSV file.
 
         Returns:
-        ------------
-        pandas.DataFrame
-            DataFrame containing the loaded data.
+            pd.DataFrame: DataFrame containing the loaded data.
+
+        Raises:
+            FileNotFoundError: If the CSV file does not exist.
         """
-        try:
-            file_path = os.path.join(self.to_load_path, "to_load_" + file_name + ".csv")
-            if os.path.exists(file_path):
-                return pd.read_csv(file_path)
-            else:
-                raise FileNotFoundError(f"The file {file_path} does not exist.")
-        except Exception as e:
-            print(f"An error occurred while loading from CSV: {e}")
-            return pd.DataFrame()
+        file_path = os.path.join(self.to_load_path, f"to_load_{file_name}.csv")
+        if os.path.exists(file_path):
+            return pd.read_csv(file_path)
+        raise FileNotFoundError(f"The file {file_path} does not exist.")
 
     def save_to_csv(self, df, file_name):
         """
         Saves a DataFrame to a CSV file.
 
         Parameters:
-        ------------
-        df: pandas.DataFrame
-            The DataFrame to save.
-        file_name: String
-            The name of the CSV file to save data to.
+            df (pd.DataFrame): The DataFrame to save.
+            file_name (str): Name of the CSV file.
         """
-        try:
-            file_path = os.path.join(self.to_load_path, "to_load_" + file_name + ".csv")
-            df.to_csv(file_path, index=False)
-        except Exception as e:
-            print(f"An error occurred while saving to CSV: {e}")
-
-    ########################################
-    # -- Starburst Query Execution Section --#
-    ########################################
+        df.to_csv(
+            os.path.join(self.to_load_path, f"to_load_{file_name}.csv"), index=False
+        )
 
     def query_run_starburst(self):
         """
-        Runs the query on the Starburst database and returns the result in a DataFrame.
+        Runs the SQL query on the Starburst database.
 
         Returns:
-        ------------
-        pandas.DataFrame
-            Returns a DataFrame containing the query results.
+            pd.DataFrame: DataFrame containing the query results.
         """
-        try:
-            if self.load_from_to_load_file:
-                return self.load_from_csv(self.load_from_to_load_file)
-
-            self.log_query(self.tp__read_query, "starburst")
-
-            df = pd.DataFrame()
-
-            # Credentials
-            USER = self.credentials["starbust_user"]
-            HOST = self.credentials["starbust_host"]
-            PORT = self.credentials["starbust_port"]
-
-            # Create connection
-            conn_details = {
-                "host": HOST,
-                "port": PORT,
-                "user": USER,
-                "http_scheme": "https",
-                "auth": trino.auth.OAuth2Authentication(),
-            }
-
-            # Execute query
-            conn = trino.dbapi.connect(**conn_details)
+        if self.load_from_to_load_file:
+            return self.load_from_csv(self.load_from_to_load_file)
+        self.log_query(self.tp__read_query, "starburst")
+        conn_details = {
+            "host": self.credentials["starbust_host"],
+            "port": self.credentials["starbust_port"],
+            "user": self.credentials["starbust_user"],
+            "http_scheme": "https",
+            "auth": trino.auth.OAuth2Authentication(),
+        }
+        with trino.dbapi.connect(**conn_details) as conn:
             df = pd.read_sql(self.tp__read_query, conn)
-            conn.close()
-
-            # Save output if specified
-            if self.to_load_file:
-                self.save_to_csv(df, self.to_load_file)
-
-            return df
-        except Exception as e:
-            print(f"An error occurred while running the Starburst query: {e}")
-            return pd.DataFrame()
-
-    ####################################
-    # -- MySQL Query Execution Section --#
-    ####################################
+        if self.to_load_file:
+            self.save_to_csv(df, self.to_load_file)
+        return df
 
     def query_run_livedb(self):
         """
-        Runs the query on the LiveDB (MySQL) database and returns the result in a DataFrame.
+        Runs the SQL query on the LiveDB (MySQL) database.
 
         Returns:
-        ------------
-        pandas.DataFrame
-            Returns a DataFrame containing the query results.
+            pd.DataFrame: DataFrame containing the query results.
         """
-        try:
-            if self.load_from_to_load_file:
-                return self.load_from_csv(self.load_from_to_load_file)
-
-            self.log_query(self.tp__read_query, "livedb")
-
-            df = pd.DataFrame()
-            # Credentials
-            USER = self.credentials["livedb_user"]
-            PW = self.credentials["livedb_pw"]
-            HOST = self.credentials["livedb_host"]
-            PORT = self.credentials["livedb_port"]
-            DB = self.credentials["livedb_database"]
-
-            # Create connection
-            conn_details = {
-                "host": HOST,
-                "port": PORT,
-                "user": USER,
-                "password": PW,
-                "database": DB,
-            }
-
-            # Perform the query
-            conn = mysql.connector.connect(**conn_details)
+        if self.load_from_to_load_file:
+            return self.load_from_csv(self.load_from_to_load_file)
+        self.log_query(self.tp__read_query, "livedb")
+        conn_details = {
+            "host": self.credentials["livedb_host"],
+            "port": self.credentials["livedb_port"],
+            "user": self.credentials["livedb_user"],
+            "password": self.credentials["livedb_pw"],
+            "database": self.credentials["livedb_database"],
+        }
+        with mysql.connector.connect(**conn_details) as conn:
             cursor = conn.cursor()
             cursor.execute(self.tp__read_query)
-            query_result = cursor.fetchall()
-
-            # Fetch Data
             columns_names = [desc[0] for desc in cursor.description]
-            df = pd.DataFrame(query_result, columns=columns_names)
-
-            # Close connection
-            conn.close()
-            cursor.close()
-
-            # Save output if specified
-            if self.to_load_file:
-                self.save_to_csv(df, self.to_load_file)
-
-            return df
-        except Exception as e:
-            print(f"An error occurred while running the LiveDB query: {e}")
-            return pd.DataFrame()
-
-    ####################################
-    # -- Query Logging Section --#
-    ####################################
+            df = pd.DataFrame(cursor.fetchall(), columns=columns_names)
+        if self.to_load_file:
+            self.save_to_csv(df, self.to_load_file)
+        return df
 
     def log_query(self, query, db_type):
         """
-        Logs the query to a file in the query_logs directory.
+        Logs the SQL query to a file.
 
         Parameters:
-        ------------
-        query: String
-            The SQL query to be logged.
-        db_type: String
-            The type of database being queried (e.g., 'starburst' or 'livedb').
+            query (str): The SQL query to log.
+            db_type (str): Type of database (e.g., 'starburst' or 'livedb').
         """
-        try:
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            log_file = os.path.join(
-                self.query_logs_path, f"{db_type}_query_{timestamp}.sql"
-            )
-
-            with open(log_file, "w") as f:
-                f.write(query)
-        except Exception as e:
-            print(f"An error occurred while logging the query: {e}")
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        log_file = os.path.join(
+            self.query_logs_path, f"{db_type}_query_{timestamp}.sql"
+        )
+        with open(log_file, "w") as f:
+            f.write(query)
