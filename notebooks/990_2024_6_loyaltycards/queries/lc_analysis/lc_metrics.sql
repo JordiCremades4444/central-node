feb74c7801fc3d203cd9ba0ca0b51e41e03d0852
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-09-18 00:48:07 +0200

    new version of lc analysis minimalist version

--- notebooks/990_2024_6_loyaltycards/queries/lc_analysis/lc_metrics.sql
+++ notebooks/990_2024_6_loyaltycards/queries/lc_analysis/lc_metrics.sql
@@ -8,7 +8,7 @@ with calendar_dates as (
 ,group_calendar_dates as (
     select
         calendar_date
-    from unnest(sequence(date_add('day', -30, date({start_date})),date({end_date}),interval '1' day)) as dates (calendar_date)
+    from unnest(sequence(date_add('day', {previous_days_start_date}, date({start_date})),date({end_date}),interval '1' day)) as dates (calendar_date)
     where 1=1
 )
 
@@ -22,18 +22,47 @@ with calendar_dates as (
         and user_type = 'Customer'
 )
 
-,lc_groups as (
+,customer_exposure as (
     select
         allocation_key as customer_id
         ,variant
         ,first_exposure_datetime as start_time
-        ,coalesce(lag(first_exposure_datetime) over (partition by allocation_key order by first_exposure_datetime desc), current_date) as end_time
+        ,coalesce(lag(first_exposure_datetime) over (partition by allocation_key order by first_exposure_datetime desc), cast({end_date} as timestamp)) as end_time
     from delta.mlp__experiment_first_exposure__odp.first_exposure
     inner join not_glovo_employees
         on first_exposure.allocation_key = not_glovo_employees.customer_id
     where 1=1
         and p_first_exposure_date in (select calendar_date from group_calendar_dates)
         and experiment_toggle_id = 'SONIC_LOYALTY_CARDS_NEW'
+)
+
+,first_exposure as (
+    select 
+        customer_id, 
+        min(first_exposure_datetime) as first_exposure_datetime
+    from delta.mlp__experiment_first_exposure__odp.first_exposure
+    inner join not_glovo_employees
+        on first_exposure.allocation_key = not_glovo_employees.customer_id
+    where 1=1
+        and p_first_exposure_date in (select calendar_date from group_calendar_dates)
+        and experiment_toggle_id = 'SONIC_LOYALTY_CARDS_NEW'
+    group by 1
+)
+
+,not_exposed_period as (
+    select 
+        customer_id, 
+        'Not Exposed' as variant,  -- or any placeholder value you want to indicate no exposure
+        cast(({start_date}) as timestamp) as start_time,
+        fe.first_exposure_datetime AS end_time
+    from first_exposure fe
+)
+
+,lc_groups as (
+    select * from not_exposed_period
+    union all
+    select * from customer_exposure
+    order by customer_id, start_time
 )
 
 ,groceries_stores_ids as (
@@ -50,7 +79,7 @@ with calendar_dates as (
     select distinct
         trim(store_name) as store_name
         ,store_id as store_id
-    from delta.mfc__temp_order_loyalty_cards__odp.temp_order_loyalty_cards_enriched  as order_loyalty_cards
+    from delta.mfc__temp_order_loyalty_cards__odp.temp_order_loyalty_cards_enriched as order_loyalty_cards
     where 1=1
         and loyalty_card is not null
         and date(p_creation_date) in (select calendar_date from calendar_dates)
@@ -60,19 +89,20 @@ with calendar_dates as (
 ,n_orders_gmv_all_groceries as (
     select
         od.p_creation_date
-        ,lcg.variant as variant
+        ,coalesce(lcg.variant, 'Not found') as variant
         ,count(distinct od.customer_id) as customers_all_groceries
         ,count(distinct od.order_id) as order_all_groceries
         ,coalesce(sum(case when (od.order_final_status = 'DeliveredStatus') then od.order_total_purchase_eur  else null end), 0) as gmv_delivered_all_groceries
     from delta.central_order_descriptors_odp.order_descriptors_v2 od
-    inner join lc_groups lcg
+    left join lc_groups lcg
         on od.customer_id = lcg.customer_id
         and od.order_created_at >= lcg.start_time 
-        and od.order_created_at < lcg.end_time
+        and od.order_created_at <= lcg.end_time
     where 1=1
         and store_id in (select store_id from groceries_stores_ids) --look at all stores
-        --and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
+        and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
         and date(p_creation_date) in (select calendar_date from calendar_dates)
+        and od.order_final_status = 'DeliveredStatus'
     group by 1,2
 )
 
@@ -80,19 +110,19 @@ with calendar_dates as (
     select
         od.p_creation_date
         ,od.store_id
-        ,lcg.variant as variant
+        ,coalesce(lcg.variant, 'Not found') as variant
         ,count(distinct od.customer_id) as customers_all_orders_from_lcs
         ,count(distinct od.order_id) as orders_all_orders_from_lcs
         ,sum(od.order_total_purchase_eur) as gmv_delivered_all_orders_from_lcs
     from delta.central_order_descriptors_odp.order_descriptors_v2 od
-    inner join lc_groups lcg
+    left join lc_groups lcg
         on od.customer_id = lcg.customer_id
         and od.order_created_at >= lcg.start_time 
         and od.order_created_at < lcg.end_time
     where 1=1
-        and store_id in (select store_id from loyalty_card_stores) --look at stores that showed at leas a lc order
+        and store_id in (select store_id from loyalty_card_stores) --look at stores that showed at least a lc order
         and date(p_creation_date) in (select calendar_date from calendar_dates)
-        --and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
+        and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
         and od.order_final_status = 'DeliveredStatus'
     group by 1,2,3
 )
@@ -101,7 +131,7 @@ with calendar_dates as (
     select
         od.p_creation_date
         ,od.store_id
-        ,lcg.variant as variant
+        ,coalesce(lcg.variant, 'Not found') as variant
         ,count(distinct od.customer_id) as customers_lc_orders_from_lcs
         ,count(distinct od.order_id) as orders_lc_orders_from_lcs
         ,sum(od.order_total_purchase_eur) as gmv_delivered_lc_orders_from_lcs
@@ -109,7 +139,7 @@ with calendar_dates as (
     inner join delta.central_order_descriptors_odp.order_descriptors_v2 od
         on lce.p_creation_date = od.p_creation_date
         and lce.order_id = od.order_id
-    inner join lc_groups lcg
+    left join lc_groups lcg
         on od.customer_id = lcg.customer_id
         and od.order_created_at >= lcg.start_time 
         and od.order_created_at < lcg.end_time
@@ -117,7 +147,7 @@ with calendar_dates as (
         and loyalty_card is not null
         and od.store_id in (select store_id from loyalty_card_stores) --look at stores that showed at leas a lc order
         and date(od.p_creation_date) in (select calendar_date from calendar_dates)
-        --and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
+        and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
         and od.order_final_status = 'DeliveredStatus'
     group by 1,2,3
 )
@@ -126,7 +156,7 @@ with calendar_dates as (
     select
         sa.p_creation_date
         ,sa.store_id
-        ,lcg.variant as variant
+        ,coalesce(lcg.variant, 'Not found') as variant
         ,count(distinct sa.dynamic_session_id) as sa_events_from_lcs
         ,count(distinct case when oc.dynamic_session_id is not null then oc.dynamic_session_id else null end) as cvr_events_from_lcs
     from delta.customer_behaviour_odp.enriched_custom_event__store_accessed_v3 sa
@@ -135,22 +165,22 @@ with calendar_dates as (
         and sa.creation_time <= oc.creation_time
         and sa.store_id = oc.store_id
         and sa.p_creation_date = oc.p_creation_date
-    inner join lc_groups lcg
+    left join lc_groups lcg
         on sa.customer_id = lcg.customer_id
         and sa.creation_time >= lcg.start_time 
         and sa.creation_time < lcg.end_time
     where 1=1
         and sa.store_id in (select store_id from loyalty_card_stores) --look at stores that showed at leas a lc order
         and date(sa.p_creation_date) in (select calendar_date from calendar_dates)
-        --and od.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
+        and sa.customer_id in (select customer_id from not_glovo_employees) --with the inner join is enough
     group by 1,2,3
 )
-
 
 select 
     --cvr metrics
     cvr.p_creation_date
     ,cvr.store_id
+    ,loyalty_card_stores.store_name
     ,cvr.variant
     ,cvr.sa_events_from_lcs
     ,cvr.cvr_events_from_lcs
@@ -178,3 +208,5 @@ left join n_orders_gmv_lc_stores
 left join n_orders_gmv_all_groceries
     on cvr.p_creation_date = n_orders_gmv_all_groceries.p_creation_date
     and cvr.variant = n_orders_gmv_all_groceries.variant
+left join loyalty_card_stores 
+    on cvr.store_id = loyalty_card_stores.store_id
