8402d145b24c349b2bcaefeb104d6b1307b72cac
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-10-03 02:03:38 +0200

    animation project

---
+++ notebooks/982_2024_10_02_plot_animation/queries/plot_animation_v2.sql
@@ -0,0 +1,20 @@
+with calendar_dates as (select
+    calendar_date
+    from unnest(sequence(
+        date({start_date}),
+        date({end_date}),
+        interval '1' day
+    )) as dates (calendar_date)
+    where true
+)
+
+select 
+    od.order_country_code,
+    count(distinct od.order_id) as n_orders,
+    count(distinct od.customer_id) as n_customers 
+from delta.central_order_descriptors_odp.order_descriptors_v2 od
+inner join calendar_dates
+    on od.p_creation_date = calendar_dates.calendar_date
+where true
+group by 1
+order by 1,2,3
