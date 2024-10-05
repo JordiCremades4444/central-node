eeb01a23db745cc11e24c4358d2cca747073fed9
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-10-05 14:50:42 +0200

    minimalist visualization

--- src/dataframe_visualizer.py
+++ src/dataframe_visualizer.py
@@ -8,8 +8,6 @@ class DataFrameVisualizer:
 
     Attributes:
         dataframe (pd.DataFrame): The pandas DataFrame to visualize.
-        colors (dict): A dictionary of color names and their corresponding hex codes.
-        fig_size (tuple): The default figure size (width, height) for plots.
     """
 
     def __init__(self, dataframe):
@@ -77,72 +75,64 @@ class DataFrameVisualizer:
             raise ValueError("The number of styles provided does not match the number of columns.")
         return [self.styles.get(style, self.styles['-']) for style in styles]
     
-    # =====================================
-    # Plot
-    # =====================================  
-    def plot(self, method='static', figure_params=None, plot_configs=None):
+    def plot(self, figure_params=None, plot_params=None):
         """
-        Handles the entire flow of figure creation and plotting based on the method.
-
+        Handles the entire flow of figure creation and plotting.
+        
         Parameters:
-            method (str): Type of plotting ('static'). Default is 'static'.
             figure_params (dict, optional): Dictionary of parameters for create_figure method, including:
                                             - n_plots (int): Number of subplots (axes) to create (default: 1).
                                             - fig_length (int): The length of the figure (default: 10).
                                             - fig_height (int): The height of the figure (default: 6).
                                             - x_rotation (int): Rotation angle for x-axis labels (default: 45).
-                                            - share_x (bool): If True, subplots share the x-axis (default: False).
-                                            - share_y (bool): If True, subplots share the y-axis (default: False).
-            plot_configs (list of dict, optional): A list of dictionaries, where each dictionary contains
+                                            - x_limit (tuple): Tuple of (x_min, x_max) for x-axis limits.
+                                            - y_limit (tuple): Tuple of (y_min, y_max) for y-axis limits.
+            plot_params (list of dict, optional): A list of dictionaries, where each dictionary contains
                                                 parameters for each subplot, including:
-                                                - plot_type (str): 'lineplot' or 'scatterplot'
-                                                - x_column (str): The x-axis column
-                                                - y_columns (list of str): List of y-axis columns
-                                                - ax (matplotlib.axes.Axes): The axis object to plot on.
+                                                - plot_type (str): Type of plot ('lineplot' or 'scatterplot').
+                                                - x_column (str): The x-axis column.
+                                                - y_columns (list of str): List of y-axis columns.
                                                 - colors (list of str, optional): List of colors for the plot.
                                                 - styles (list of str, optional): List of line/marker styles.
         """
-        if method == 'static':
-            # Default figure parameters if none provided
-            if figure_params is None:
-                figure_params = {
-                    'n_plots': 1,
-                    'fig_length': 10,
-                    'fig_height': 6,
-                    'x_rotation': 45,
-                    'share_x': False,
-                    'share_y': False
-                }
+        
+        # Default figure parameters if none provided
+        if figure_params is None:
+            figure_params = {
+                'n_plots': 1,
+                'fig_length': 10,
+                'fig_height': 6,
+                'x_rotation': 45,
+                'x_limits': None,
+                'y_limits': None,
+            }
 
-            # Create the figure and axes
-            fig, axes = self.create_figure(**figure_params)
-            
-            # Ensure axes is always a list even for a single subplot
-            if figure_params['n_plots'] == 1:
-                axes = [axes]
-            
-            # Loop through each plot configuration
-            for i, plot_config in enumerate(plot_configs):
-                ax = axes[i]
-                plot_type = plot_config.get('plot_type')
-                x_column = plot_config.get('x_column')
-                y_columns = plot_config.get('y_columns')
-                colors = plot_config.get('colors', None)
-                styles = plot_config.get('styles', None)
-                
-                # Call the appropriate plot method
-                if plot_type == 'lineplot':
-                    self.multiple_variable_lineplot(x_column=x_column, y_columns=y_columns, ax=ax, colors=colors, styles=styles)
-                elif plot_type == 'scatterplot':
-                    self.multiple_variable_scatterplot(x_column=x_column, y_columns=y_columns, ax=ax, colors=colors)
-            
-            # Display the plot
-            plt.show()
+        # Create the figure and axes
+        fig, axes = self.create_figure(**figure_params)
+        
+        # Ensure axes is always a list even for a single subplot
+        if figure_params['n_plots'] == 1:
+            axes = [axes]
 
-    # =====================================
-    # Figure
-    # =====================================
-    def create_figure(self, n_plots=1, fig_length=10, fig_height=6, x_rotation=45, share_x=False, share_y=False, x_min=None, x_max=None, y_min=None, y_max=None):
+        # Loop through each plot configuration
+        for i, config in enumerate(plot_params):
+            ax = axes[i]
+            plot_type = config.get('plot_type')
+            x_column = config.get('x_column')
+            y_columns = config.get('y_columns')
+            colors = config.get('colors', None)
+            styles = config.get('styles', None)
+            legend = config.get('legend')
+
+            # Call static plotting method
+            if plot_type == 'lineplot':
+                self.multiple_variable_lineplot(x_column=x_column, y_columns=y_columns, ax=ax, colors=colors, styles=styles, legend=legend)
+            elif plot_type == 'scatterplot':
+                self.multiple_variable_scatterplot(x_column=x_column, y_columns=y_columns, ax=ax, colors=colors, legend=legend)
+
+        plt.show()
+
+    def create_figure(self, n_plots=1, fig_length=10, fig_height=6, x_rotation=45, share_x=False, share_y=False, x_limits=None, y_limits=None, title=None):
         """
         Defines the figure and axes based on the number of plots.
         Sets the axes properties such as labels and x-axis rotation.
@@ -155,10 +145,9 @@ class DataFrameVisualizer:
             x_rotation (int, optional): Rotation angle for x-axis labels (default: 45).
             share_x (bool, optional): If True, subplots share the x-axis (default: False).
             share_y (bool, optional): If True, subplots share the y-axis (default: False).
-            x_min (float, optional): Minimum x-axis value.
-            x_max (float, optional): Maximum x-axis value.
-            y_min (float, optional): Minimum y-axis value.
-            y_max (float, optional): Maximum y-axis value.
+            x_limits (tuple, optional): Tuple of (x_min, x_max) to set the x-axis limits. If None, axis is set automatically.
+            y_limits (tuple, optional): Tuple of (y_min, y_max) to set the y-axis limits. If None, axis is set automatically.
+            title (str, optional): Title of the figure.
         """
         self.fig_size = (fig_length, fig_height)
 
@@ -166,16 +155,16 @@ class DataFrameVisualizer:
             fig, ax = plt.subplots(figsize=self.fig_size)
             ax.tick_params(axis='x', rotation=x_rotation)
 
-            # Set axis limits independently
-            if x_min is not None:
-                ax.set_xlim(left=x_min)
-            if x_max is not None:
-                ax.set_xlim(right=x_max)
-            if y_min is not None:
-                ax.set_ylim(bottom=y_min)
-            if y_max is not None:
-                ax.set_ylim(top=y_max)
+            # Set axis limits together for x and y
+            if x_limits is not None and len(x_limits) == 2:
+                ax.set_xlim(left=x_limits[0], right=x_limits[1])
+            if y_limits is not None and len(y_limits) == 2:
+                ax.set_ylim(bottom=y_limits[0], top=y_limits[1])
 
+            if title:
+                print(title)
+                fig.suptitle(title, fontsize=16, y=0.02)  # y controls position, adjust as necessary
+            
             return fig, ax
 
         else:
@@ -185,22 +174,23 @@ class DataFrameVisualizer:
             for ax in axs:
                 ax.tick_params(axis='x', rotation=x_rotation)
 
-                # Set axis limits independently
-                if x_min is not None:
-                    ax.set_xlim(left=x_min)
-                if x_max is not None:
-                    ax.set_xlim(right=x_max)
-                if y_min is not None:
-                    ax.set_ylim(bottom=y_min)
-                if y_max is not None:
-                    ax.set_ylim(top=y_max)
+                # Set axis limits together for x and y
+                if x_limits is not None and len(x_limits) == 2:
+                    ax.set_xlim(left=x_limits[0], right=x_limits[1])
+                if y_limits is not None and len(y_limits) == 2:
+                    ax.set_ylim(bottom=y_limits[0], top=y_limits[1])
 
+            if title:
+                print(title)
+                fig.suptitle(title, fontsize=11, y=0)  # y controls position, adjust as necessary
+            
             return fig, axs
+   
 
     # =====================================
     # Plots methods
     # =====================================
-    def multiple_variable_lineplot(self, x_column, y_columns, ax, colors=None, styles=None):
+    def multiple_variable_lineplot(self, x_column, y_columns, ax, colors=None, styles=None, legend=True):
         """
         Creates a line plot for multiple y variables against an x variable.
         If the x_column is a pd.datetime, then the x-axis will not be too crowded.
@@ -212,17 +202,20 @@ class DataFrameVisualizer:
             colors (list, optional): List of colors for each y-column.
             styles (list, optional): List of line styles for each y-column.
         """
+        # Use the passed dataframe, or default to self.dataframe if not provided
         self._validate_columns([x_column] + y_columns)
         colors = self._get_colors(colors, len(y_columns))
         styles = self._get_styles(styles, len(y_columns))
 
         for y_column, color, style in zip(y_columns, colors, styles):
-            ax.plot(self.dataframe[x_column], self.dataframe[y_column], color=color, linestyle=style)
+            ax.plot(self.dataframe[x_column], self.dataframe[y_column], color=color, linestyle=style, label=y_column)
 
-    def multiple_variable_scatterplot(self, x_column, y_columns, ax, colors=None):
+        if legend:
+            ax.legend(loc='best')
+
+    def multiple_variable_scatterplot(self, x_column, y_columns, ax, colors=None, legend=True):
         """
         Creates a scatter plot for multiple y variables against an x variable.
-        If the x_column is a pd.datetime, then the x-axis will not be too crowded.
 
         Parameters:
             x_column (str): Column name for x-axis.
@@ -230,8 +223,12 @@ class DataFrameVisualizer:
             ax (matplotlib.axes.Axes): Axis object to plot on.
             colors (list, optional): List of colors for each y-column.
         """
+        # Use the passed dataframe, or default to self.dataframe if not provided
         self._validate_columns([x_column] + y_columns)
         colors = self._get_colors(colors, len(y_columns))
 
         for y_column, color in zip(y_columns, colors):
-            ax.scatter(self.dataframe[x_column], self.dataframe[y_column], color=color)
+            ax.scatter(self.dataframe[x_column], self.dataframe[y_column], color=color, label=y_column)
+
+        if legend:
+            ax.legend(loc='best')
