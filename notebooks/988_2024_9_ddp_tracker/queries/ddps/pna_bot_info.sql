7cf80d716f026bf67a2cf3b95d205ef614395d6a
JordiCremades4444 <jordicremadesrosell@gmail.com>
2024-10-22 10:18:27 +0200

    minimalist

--- notebooks/988_2024_9_ddp_tracker/queries/ddps/pna_bot_info.sql
+++
@@ -1,3 +0,0 @@
-select 
-    count(*) as count_pna_bot_info
-from delta.central__pna_bot__odp.pna_bot_info
--- notebooks/988_2024_9_ddp_tracker/queries/ddps/replacement_engine_info.sql
+++
@@ -1,3 +0,0 @@
-select 
-    count(*) as count_replacement_engine_info
-from delta.central__replacement_engine_dp__odp.replacement_engine_info
